690a16682647ae464b08d4a3bae40d15a7587981
Jacob Yang <yajacob@gmail.com>
2022-10-10 13:40:26 -0700

    cookie time - 7 days

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $person = strtolower($_GET['person']);
 
 if($person) {
   if($person == "0153") {
-    setcookie("access_bible", "true", time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("access_bible", "true", time() + (86400 * 7), "/"); // 86400 = 1 day
     header("Location: /index.php");
   } else {
     header("Location: /login.php");
