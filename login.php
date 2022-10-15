28025e8a50f1e16a3203e64be52bed552993fea1
Jacob Yang <yajacob@gmail.com>
2022-10-15 10:42:38 -0700

    add member

--- login.php
+++ login.php
@@ -2,11 +2,15 @@
 $person = strtolower($_GET['person']);
 
 // person list array
-$person_list = ["0728", "lordjesus", "0153", "6644"];
+$person_list = ["0728", "lordjesus", "0153", "6644", "1014"];
 
-if($person) {
+// 0728 장모님
+// 6644 우자매님
+// 1014 종은자매님
+
+if ($person) {
   // check if person is in the list
-  if(in_array($person, $person_list)) {
+  if (in_array($person, $person_list)) {
     setcookie("access_bible", "true", time() + (86400 * 7), "/"); // 86400 = 1 day
     header("Location: /index.php");
   } else {
